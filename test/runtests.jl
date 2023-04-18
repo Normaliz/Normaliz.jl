9b80b09939f380361a9d4c0ce064f5ab92e7c43a
Max Horn <max@quendi.de>
2023-04-18 15:50:07 +0200

    Update tests to not crash

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,17 +1,25 @@
 using Test
 using Normaliz
 
-@testset "basic int test" begin
-  xx = Normaliz.NmzMatrix{Int}([1 2 ; 3 5])
+@testset "basic LongCone test" begin
+  xx = Normaliz.NmzMatrix{Normaliz.NmzRational}([1 2 ; 3 5])
+  yy = Normaliz.LongCone( Dict( :cone => xx ) )
+  Normaliz.get_matrix_cone_property( yy, "ExtremeRays" )
+  Normaliz.get_matrix_cone_property( yy, "SupportHyperplanes" )
+end
+
+@testset "basic IntCone test" begin
+  xx = Normaliz.NmzMatrix{Normaliz.NmzRational}([1 2 ; 3 5])
   yy = Normaliz.IntCone( Dict( :cone => xx ) )
   Normaliz.get_matrix_cone_property( yy, "ExtremeRays" )
   Normaliz.get_matrix_cone_property( yy, "SupportHyperplanes" )
 end
 
-@testset "basic renf test" begin
-  r = Normaliz.RenfClass("a4-5a2+5", "a", "1.9021+/-0.01")
-  xx = Normaliz.NmzMatrix{Normaliz.Renf}(r,[1 2 ; 3 5])
-  yy = Normaliz.RenfCone( Dict( :cone => xx ) )
-  Normaliz.get_matrix_cone_property( yy, "ExtremeRays" )
-  Normaliz.get_matrix_cone_property( yy, "SupportHyperplanes" )
-end
+# TODO: reactivate these tests once Renf support is back
+#@testset "basic renf test" begin
+#  r = Normaliz.RenfClass("a4-5a2+5", "a", "1.9021+/-0.01")
+#  xx = Normaliz.NmzMatrix{Normaliz.Renf}(r,[1 2 ; 3 5])
+#  yy = Normaliz.RenfCone( Dict( :cone => xx ) )
+#  Normaliz.get_matrix_cone_property( yy, "ExtremeRays" )
+#  Normaliz.get_matrix_cone_property( yy, "SupportHyperplanes" )
+#end
